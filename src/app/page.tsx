a22d12242facac72cf8a687f14a9dc4b14085969
Nikita Koynov <nikitakoynov@gmail.com>
2025-04-08 23:23:39 +0300

    connect to mongodb

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
-export default function Index() {
+import mongoClient from '~/lib/mongodb';
+
+export default async function Index() {
+  const isConnected = await testDatabaseConnection();
+
   return (
     <div>
-      <p className="font-sans antialiased">Home</p>
+      <p>Home</p>
+      <p>{isConnected ? '' : 'not'} connected</p>
     </div>
   );
+}
+
+export async function testDatabaseConnection() {
+  try {
+    await mongoClient.db('admin').command({ ping: 1 });
+    console.log('Pinged your deployment. You successfully connected to MongoDB!'); // because this is a server action, the console.log will be outputted to your terminal not in the browser
+    return true;
+  } catch (e) {
+    console.error(e);
+    return false;
+  }
 }
---
+++ src/lib/mongodb.ts
@@ -0,0 +1,31 @@
+import { MongoClient, MongoClientOptions } from 'mongodb';
+
+if (!process.env.MONGODB_URI) {
+  throw new Error('Missing environment variable: "MONGODB_URI"');
+}
+
+const uri = process.env.MONGODB_URI;
+const options: MongoClientOptions = {
+  appName: `next-shop.${process.env.NODE_ENV.toLowerCase()}`,
+};
+
+let client: MongoClient;
+
+if (process.env.NODE_ENV === 'development') {
+  // In development mode, use a global variable so that the value
+  // is preserved across module reloads caused by HMR (Hot Module Replacement).
+  // eslint-disable-next-line prefer-const
+  let globalWithMongo = global as typeof globalThis & {
+    _mongoClient?: MongoClient;
+  };
+
+  if (!globalWithMongo._mongoClient) {
+    globalWithMongo._mongoClient = new MongoClient(uri, options);
+  }
+  client = globalWithMongo._mongoClient;
+} else {
+  // In production mode, it's best to not use a global variable.
+  client = new MongoClient(uri, options);
+}
+
+export default client;
