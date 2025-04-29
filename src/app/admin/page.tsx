7ea7efb00bcfe64b3d329ee417c552bab7ba0082
Nikita Koynov <nikitakoynov@gmail.com>
2025-04-29 15:47:41 +0300

    update schema

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,7 +1,10 @@
+import { redirect } from 'next/navigation';
+
 export default function Admin() {
-  return (
-    <div className="flex flex-col gap-4 px-4 py-4 md:gap-6 md:py-6">
-      <p>Admin dashboard</p>
-    </div>
-  );
+  redirect('/admin/products');
+  // return (
+  //   <div className="flex flex-col gap-4 px-4 py-4 md:gap-6 md:py-6">
+  //     <p>Admin dashboard</p>
+  //   </div>
+  // );
 }
--- src/lib/auth/server.ts
+++ src/lib/auth/server.ts
@@ -2,7 +2,7 @@ import { betterAuth } from 'better-auth';
 import { mongodbAdapter } from 'better-auth/adapters/mongodb';
 import { nextCookies } from 'better-auth/next-js';
 
-import mongoClient from '~/lib/mongodb/client';
+import { getDefaultDb } from '~/lib/mongodb/db';
 
 if (!process.env.GOOGLE_CLIENT_ID) {
   throw new Error('Missing environment variable: "GOOGLE_CLIENT_ID"');
@@ -11,10 +11,8 @@ if (!process.env.GOOGLE_CLIENT_SECRET) {
   throw new Error('Missing environment variable: "GOOGLE_CLIENT_SECRET"');
 }
 
-const db = mongoClient.db();
-
 export const auth = betterAuth({
-  database: mongodbAdapter(db),
+  database: mongodbAdapter(getDefaultDb()),
   socialProviders: {
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID,
